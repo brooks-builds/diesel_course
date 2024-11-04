8076532497cb1da7dda518e1259c9d6e1645fcc9
Brookzerker <brooks@brookzerker.com>
2024-11-04 11:46:32 -0700

    creating models lesson

--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,6 @@
 use crate::schema::pets;
 use crate::schema::species;
+use chrono::NaiveDateTime;
 use diesel::prelude::*;
 
 #[derive(Debug, Insertable)]
@@ -13,4 +14,22 @@ pub struct CreateSpecies {
 pub struct CreatePet {
     pub name: String,
     pub species_id: i32,
+}
+
+#[derive(Debug, Identifiable, Queryable, Selectable)]
+#[diesel(table_name = species)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct Species {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Debug, Identifiable, Queryable, Selectable, Associations)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+#[diesel(belongs_to(Species))]
+pub struct Pet {
+    pub id: i32,
+    pub name: String,
+    pub species_id: i32,
+    pub last_fed: Option<NaiveDateTime>,
 }
