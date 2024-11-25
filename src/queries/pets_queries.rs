3a1838dc03078860703391dcad91eb08df8e507e
Brookzerker <brooks@brookzerker.com>
2024-11-25 12:04:13 -0700

    added query debug for debug lesson

--- src/queries/pets_queries.rs
+++ src/queries/pets_queries.rs
@@ -6,17 +6,15 @@ use eyre::{Context, Result};
 pub fn get_all_pets(db: &mut PgConnection) -> Result<Vec<Pet>> {
     use crate::schema::pets::dsl::pets;
 
-    let query = pets.select(Pet::as_select());
-
-    println!("get all pets query: {}", debug_query::<Pg, _>(&query));
-
-    query.load(db).context("Getting all pets")
+    pets.select(Pet::as_select())
+        .load(db)
+        .context("Getting all pets")
 }
 
 pub fn get_pet_by_id(db: &mut PgConnection, id: i32) -> Result<Option<Pet>> {
-    pets::table
-        .find(id)
-        .first(db)
-        .optional()
-        .context("getting pet by id")
+    let pet = pets::table.find(id);
+
+    println!("**pet query:**\n{}\n", debug_query::<Pg, _>(&pet));
+
+    pet.first(db).optional().context("getting pet by id")
 }
